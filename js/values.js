b6034264b15ee1b72d07940f58b0ebbf152bed37
Henrique Santos <hv_santos@outlook.com>
2022-03-13 05:56:19 -0300

    remove unused variables

--- js/values.js
+++ js/values.js
@@ -4,12 +4,6 @@ const valueReal = 1.0
 const valueEuro = 5.54
 const valueBitcoin = 198787.75
 
-let valueDollartoEuro = valueDollar / valueEuro
-let valueDollartoReal = valueDollar / valueReal
-let valueDollartoBitcoin = valueDollar / valueBitcoin
-let valueRealtoDollar = valueReal / valueDollar
-let valueRealtoEuro = valueReal / valueEuro
-let valueRealtoBitcoin = valueReal / valueBitcoin
-let valueEurotoDollar = valueEuro / valueDollar
-let valueEurotoReal = valueEuro / valueReal
-let valueEurotoBitcoin = valueEuro / valueBitcoin
+const valueRealtoDollar = valueReal / valueDollar
+const valueRealtoEuro = valueReal / valueEuro
+const valueRealtoBitcoin = valueReal / valueBitcoin
